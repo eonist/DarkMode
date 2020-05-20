05c8b4281b7f857fe1d5ecd344bf0d2a06b916a5
eonist <30n1st@gmail.com>
2020-05-20 13:45:39 +0200

    Create UIImageAsset+Extension.swift

---
+++ Sources/DarkMode/common/UIImageAsset+Extension.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+public extension UIImageAsset {
+   /**
+    * Creates an image asset with registration of tht eimages with the light and dark trait collections.
+    * - Parameters:
+    * - lightModeImage: The image you want to register with the image asset with light user interface style.
+    * - darkModeImage: The image you want to register with the image asset with dark user interface style.
+    */
+   convenience init(lightModeImage: UIImage?, darkModeImage: UIImage?) {
+      self.init()
+      register(lightModeImage: lightModeImage, darkModeImage: darkModeImage)
+   }
+   /**
+    * Register an images with the light and dark trait collections respectively.
+    *- Parameters:
+    *- lightModeImage: The image you want to register with the image asset with light user interface style.
+    *- darkModeImage: The image you want to register with the image asset with dark user interface style.
+    */
+   func register(lightModeImage: UIImage?, darkModeImage: UIImage?) {
+      register(lightModeImage, for: .light)
+      register(darkModeImage, for: .dark)
+   }
+   /**
+    * Register an image with the specified trait collection.
+    * - Parameters:
+    * - image: The image you want to register with the image asset.
+    * - traitCollection: The traits to associate with image.
+    */
+   func register(_ image: UIImage?, for userInterfaceStyle: UIUserInterfaceStyle) {
+      guard let image = image else { return }
+      register(image, with: .init(userInterfaceStyle: userInterfaceStyle))
+   }
+   /**
+    * Returns the variant of the image that best matches the current trait collection. For early SDKs returns the image for light user interface style.
+    */
+   func image() -> UIImage {
+      if #available(iOS 13.0, tvOS 13.0, *) {
+         image(with: .current)
+      }
+      return image(with: .init(userInterfaceStyle: .light))
+   }
+}

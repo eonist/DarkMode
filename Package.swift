748f7943643cc941e412bf99127e312151e821d0
eonist <30n1st@gmail.com>
2023-03-27 03:59:06 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "DarkMode",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v12)], // update to ios15? and swift 5.6?
     products: [
         .library(
             name: "DarkMode",
